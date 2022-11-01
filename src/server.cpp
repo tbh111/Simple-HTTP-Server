6484d33ea94a9d4ab4819b0a989f37ad0a0f4922
tongbh111 <1308751722@qq.com>
2022-11-01 19:44:49 +0800

    sync

--- src/server.cpp
+++ src/server.cpp
@@ -1,28 +1,63 @@
 #include "server.h"
 using namespace std;
 
-HttpResponse::HttpResponse()
-{
+HttpRequest::HttpRequest() {}
 
+void HttpRequest::string2header(std::string input)
+{
+    istringstream iss(input);
+    std::string state;
+    vector<std::string> state_list;
+    while(getline(iss, state))
+    {
+        state_list.push_back(state);
+    }
+
+    std::string token;
+    vector<std::string> string_list;
+    while(getline(iss, token, '\r'))
+    {
+        string_list.push_back(token);
+        istringstream iss1(token);
+        std::string token1;
+        vector<std::string> string_list1;
+        getline(iss1, token1, ':');
+        string_list1.push_back(token1);
+        string_list1[1].erase(string_list1[1].begin());
+        request_header[string_list1[0]] = string_list1[1];
+    }
 }
 
-void HttpResponse::pack_response()
+HttpResponse::HttpResponse() {}
+
+void HttpResponse::header2string()
 {
-    response = "";
-    response += "HTTP/1.1 200 OK\r\n";
-    response += "Content-Type:text/html\r\n";
-    response += "\r\n";
-    response += "<html><head>Hello World</head></html>\r\n";
+    ostringstream oss;
+    oss << ver << " " << code << " " << state << CRLF;
+    for(auto it = response_header.begin(); it != response_header.end(); it++)
+    {
+        oss << it->first << ": " << it->second << CRLF;
+    }
+    oss << CRLF;
+    oss << body << CRLF;
+    send_message = oss.str();
 }
+
 
 Server::Server(int server_mode)
 {
     mode = server_mode;
+    STATE_CODE_MAP[200] = "OK";
+    STATE_CODE_MAP[301] = "Moved Permanently";
+    STATE_CODE_MAP[206] = "Partial Content";
+    STATE_CODE_MAP[403] = "Forbidden";
+    STATE_CODE_MAP[404] = "Not Found";
 }
+
 
 int Server::set_socket()
 {
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    s = socket(PF_INET, SOCK_STREAM, 0);
     if(s == -1)
     {
         cout <<"cannot open socket" << endl;
@@ -59,6 +94,53 @@ int Server::set_socket()
     return 0;
 }
 
+void Server::set_response_var(std::string type, std::string name)
+{
+    if(type == "html")
+    {
+        response.response_header["Content-Type"] = "text/html";
+        ifstream fd;
+        std::string full_path = string(ROOT_PATH)+"name";
+        fd.open(full_path, ios::in);
+        fd.seekg(0, std::ifstream::end);
+        int file_size =fd.tellg();
+        response.response_header["Content-Length"] = to_string(file_size);
+        response.response_header["Connection"] = "keep-alive";
+        char tmp = -1;
+        while ((tmp = fd.get())!=EOF)
+        {
+            string tmp_s;
+            tmp_s.push_back(tmp);
+            response.body += tmp_s;
+        }
+        fd.close();
+    }
+    else if(type == "mp4")
+    {
+        std::string full_path = string(ROOT_PATH) + "name";
+        int video_fd = open(full_path.c_str(), O_RDONLY);
+        int video_length = lseek(video_fd, 0, SEEK_END) - lseek(video_fd, 0, SEEK_SET);
+        response.response_header["Content-Type"] = "video/mpeg";
+        response.response_header["Accept-Ranges"] = "bytes";
+        response.response_header["Connection"] = "keep-alive";
+        response.response_header["Transfer-Encoding"] = "chunked";
+        response.response_header["Content-Length"] = std::to_string(video_length);
+
+        if(response.code == 200)
+        {
+            char video_buf[BUF_SIZE];
+            while(read(video_fd, video_buf, sizeof(video_buf)))
+            {
+                response.body += video_buf;
+            }
+        }
+        else
+        {
+
+        }
+    }
+}
+
 void Server::accept_request()
 {
     struct sockaddr_in client_addr;
@@ -73,11 +155,97 @@ void Server::accept_request()
     cout << "connect to " << ip << endl;
     fill_n(buf, BUF_SIZE, 0);
     int size = read(client_fd, buf, sizeof(buf));
-    cout << "get request: \n" << string(buf) << endl;
-    HttpResponse resp;
-    resp.pack_response();
-    send(client_fd, resp.response.c_str(), resp.response.length(), MSG_WAITALL);
+    request.string2header(buf);
+
+    response.ver = request.ver;
+    if(!access((ROOT_PATH + request.url).c_str(), 6))
+    {
+        cout << "Not Found" << endl;
+        response.code = 404;
+    }
+    else if(!access((ROOT_PATH + request.url).c_str(), 0) | !access((ROOT_PATH + request.url).c_str(), 2) |
+            !access((ROOT_PATH + request.url).c_str(), 4))
+    {
+        cout << "Forbidden" << endl;
+        response.code = 403;
+    }
+//    else if(mode == HTTP_MODE) // turn to https, not complemented yet
+//    {
+//
+//    }
+
+    else
+    {
+        int dot_pos = request.url.find(".");
+        if(dot_pos == string::npos)
+        {
+            cout << "Not Found" << endl;
+            response.code = 404;
+        }
+        else
+        {
+            std::string file_type = request.url.substr(dot_pos, request.url.length()-dot_pos);
+            cout << file_type << "file will transmit to client" << endl;
+
+            if(file_type == "mp4" && request.request_header.find("Range") != response.response_header.end())
+            {
+                cout << "Partial Content" << endl;
+                response.code = 206;
+            }
+            else
+            {
+                cout << "OK" << endl;
+                response.code = 200;
+            }
+
+            set_response_var(file_type, request.url);
+        }
+    }
+    response.state = STATE_CODE_MAP[response.code];
+    response.header2string();
+    send(client_fd, response.send_message.c_str(), response.send_message.length(), MSG_WAITALL);
+    //cout << resp.response << endl;
+    //send(client_fd, resp.response.c_str(), resp.response.length(), MSG_WAITALL);
     close(client_fd);
 }
 
 Server::~Server() = default;
+
+//void Server::pack_response() // pack header and information, then send to client
+//{
+//    response.send_message = "";
+//    response.send_message += "HTTP/1.1 200 OK\r\n";
+////    response.send_message += "Content-Type:text/html\r\n\r\n";
+//
+//    response.send_message += "Content-Type:video/mpeg\r\n";
+//    //response.send_message += "<html><head>Hello World</head></html>\r\n";
+//
+//    //for video
+//    int video_fd = open("1.mp4", O_RDONLY);
+//    int video_length = lseek(video_fd, 0, SEEK_END)- lseek(video_fd, 0, SEEK_SET);
+////    response += "Content-Length:";
+////    response += to_string(video_length);
+//    response.send_message += "\r\n";
+//    cout << response.send_message << endl;
+//    char video_buf[BUF_SIZE];
+//    if (read(video_fd, video_buf, sizeof(video_buf)))
+//    {
+//        response.send_message += video_buf;
+//    }
+//    response.send_message += "\r\n";
+////    // for text
+////    ifstream fd;
+////    fd.open("index.html", ios::in);
+////    char tmp = 0;
+////    while ((tmp = fd.get())!=EOF)
+////    {
+////        string tmp_s;
+////        tmp_s.push_back(tmp);
+////        response.send_message += tmp_s;
+////    }
+////    response.send_message += "\r\n";
+////    cout << "r" << response.send_message << endl;
+//
+//}
+
+
